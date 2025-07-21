4967f6cbec9fe4b63f4f7eb311b7772ac276bc18
pushpendra <singh16195@gmail.com>
2025-07-21 13:41:14 +0530

    movie slice creation and store update

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTE3MTFhZTYwY2FjNWQ0NDE2ZDM4NGY1MzJjZjkyMiIsIm5iZiI6MTY0NzI1NzYxNi42NjIwMDAyLCJzdWIiOiI2MjJmMjgxMDlkMmI2MzAwNzg5NWZmN2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.A5NHw0T0pQNTj_L1tVL-k8pBCe3z5Jc70xOp4VF19O4",
+  },
+};
+export const TMDB_API_URL =
+  "https://api.themoviedb.org/3/movie/now_playing?&page=1";
