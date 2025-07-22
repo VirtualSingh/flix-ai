3c248b20fc6859f53e01cdf24a5a209aa8ae51d4
pushpendra <singh16195@gmail.com>
2025-07-22 16:37:51 +0530

    env file creation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTE3MTFhZTYwY2FjNWQ0NDE2ZDM4NGY1MzJjZjkyMiIsIm5iZiI6MTY0NzI1NzYxNi42NjIwMDAyLCJzdWIiOiI2MjJmMjgxMDlkMmI2MzAwNzg5NWZmN2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.A5NHw0T0pQNTj_L1tVL-k8pBCe3z5Jc70xOp4VF19O4",
+    Authorization: import.meta.env.VITE_TMDB_TOKEN,
   },
 };
 export const TMDB_API_URL =
